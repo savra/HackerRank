4fac0d0b39f222671582b97115b7c68414b7a45a
Sergey Savrukhin <savra.sv@yandex.ru>
2023-09-20 07:14:20 +0300

    Java Int to String

---
+++ src/main/java/com/hvdbs/savra/hackerrank/solution/java/JavaIntToString.java
@@ -0,0 +1,26 @@
+package com.hvdbs.savra.hackerrank.solution.java;
+
+import com.hvdbs.savra.hackerrank.statsgenerator.HackerRankInfo;
+import com.hvdbs.savra.hackerrank.statsgenerator.enums.Difficulty;
+
+import java.util.Scanner;
+
+@HackerRankInfo(
+        difficulty = Difficulty.EASY,
+        name = "Java Int to String",
+        url = "https://www.hackerrank.com/challenges/java-int-to-string"
+)
+public class JavaIntToString {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        try {
+            int a = s.nextInt();
+        } catch (NumberFormatException e) {
+            System.out.println("Wrong answer");
+            return;
+        }
+
+        System.out.println("Good job");
+    }
+}

340a141cd9916b49b8ac96d29d21731eef1d4120
Sergey Savrukhin <savra.sv@yandex.ru>
2023-09-20 08:11:52 +0300

    Java Strings Introduction

---
+++ src/main/java/com/hvdbs/savra/hackerrank/solution/java/JavaStringsIntroduction.java
@@ -0,0 +1,24 @@
+package com.hvdbs.savra.hackerrank.solution.java;
+
+import com.hvdbs.savra.hackerrank.statsgenerator.HackerRankInfo;
+import com.hvdbs.savra.hackerrank.statsgenerator.enums.Difficulty;
+
+import java.util.Scanner;
+
+@HackerRankInfo(
+        difficulty = Difficulty.EASY,
+        name = "Java Strings Introduction",
+        url = "https://www.hackerrank.com/challenges/java-strings-introduction"
+)
+public class JavaStringsIntroduction {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        String a = s.next();
+        String b = s.next();
+
+        System.out.println(a.length() + b.length());
+        System.out.println(a.compareTo(b) > 0 ? "Yes" : "No");
+        System.out.println(a.substring(0, 1).toUpperCase() + a.substring(1) + " " + b.substring(0, 1).toUpperCase() + b.substring(1));
+    }
+}

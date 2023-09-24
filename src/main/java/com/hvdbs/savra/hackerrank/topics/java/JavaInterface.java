0701c65340518f329f49a52ea45deaa09480876c
Sergey Savrukhin <savra.sv@yandex.ru>
2023-09-24 10:21:29 +0300

    Java Interface

---
+++ src/main/java/com/hvdbs/savra/hackerrank/topics/java/JavaInterface.java
@@ -0,0 +1,54 @@
+package com.hvdbs.savra.hackerrank.topics.java;
+
+import com.hvdbs.savra.hackerrank.statsgenerator.HackerRankInfo;
+import com.hvdbs.savra.hackerrank.statsgenerator.enums.Difficulty;
+
+import java.util.Scanner;
+
+@HackerRankInfo(
+        difficulty = Difficulty.EASY,
+        name = "Java Interface",
+        url = "https://www.hackerrank.com/challenges/java-interface"
+)
+public class JavaInterface {
+    interface AdvancedArithmetic {
+        int divisor_sum(int n);
+    }
+
+    static class MyCalculator implements AdvancedArithmetic {
+        @Override
+        public int divisor_sum(int n) {
+            int sum = 0;
+
+            for (int i = 1; i <= n; i++) {
+                if (n % i == 0) {
+                    sum += i;
+                }
+            }
+
+            return sum;
+        }
+    }
+
+    public static void main(String[] args) {
+        MyCalculator my_calculator = new MyCalculator();
+        System.out.print("I implemented: ");
+        ImplementedInterfaceNames(my_calculator);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(my_calculator.divisor_sum(n) + "\n");
+        sc.close();
+    }
+
+    /*
+     *  ImplementedInterfaceNames method takes an object and prints the name of the interfaces it implemented
+     */
+    static void ImplementedInterfaceNames(Object o) {
+        Class[] theInterfaces = o.getClass().getInterfaces();
+        for (int i = 0; i < theInterfaces.length; i++) {
+            String interfaceName = theInterfaces[i].getName();
+            System.out.println(interfaceName);
+        }
+    }
+
+}

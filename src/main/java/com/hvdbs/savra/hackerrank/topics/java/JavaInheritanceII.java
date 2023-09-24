ed7f6493f59a8b2a3112c04393356ecdaecf5139
Sergey Savrukhin <savra.sv@yandex.ru>
2023-09-24 10:03:45 +0300

    Java Inheritance II

---
+++ src/main/java/com/hvdbs/savra/hackerrank/topics/java/JavaInheritanceII.java
@@ -0,0 +1,32 @@
+package com.hvdbs.savra.hackerrank.topics.java;
+
+import com.hvdbs.savra.hackerrank.statsgenerator.HackerRankInfo;
+import com.hvdbs.savra.hackerrank.statsgenerator.enums.Difficulty;
+
+@HackerRankInfo(
+        difficulty = Difficulty.EASY,
+        name = "Java Inheritance II",
+        url = "https://www.hackerrank.com/challenges/java-inheritance-2"
+)
+public class JavaInheritanceII {
+    static class Arithmetic {
+        public int add(int a, int b) {
+            return a + b;
+        }
+    }
+
+    static class Adder extends Arithmetic {
+    }
+
+    public static void main(String[] args) {
+        // Create a new Adder object
+        Adder a = new Adder();
+
+        // Print the name of the superclass on a new line
+        System.out.println("My superclass is: " + a.getClass().getSuperclass().getName());
+
+        // Print the result of 3 calls to Adder's `add(int,int)` method as 3 space-separated integers:
+        System.out.print(a.add(10, 32) + " " + a.add(10, 3) + " " + a.add(10, 10) + "\n");
+    }
+
+}

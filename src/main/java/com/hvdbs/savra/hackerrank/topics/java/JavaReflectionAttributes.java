d43983a28e3c937c7461ec1b593c4da49f10ac17
Sergey Savrukhin <savra.sv@yandex.ru>
2023-09-24 14:09:30 +0300

    Java Reflection - Attributes

---
+++ src/main/java/com/hvdbs/savra/hackerrank/topics/java/JavaReflectionAttributes.java
@@ -0,0 +1,59 @@
+package com.hvdbs.savra.hackerrank.topics.java;
+
+import com.hvdbs.savra.hackerrank.statsgenerator.HackerRankInfo;
+import com.hvdbs.savra.hackerrank.statsgenerator.enums.Difficulty;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collections;
+
+@HackerRankInfo(
+        difficulty = Difficulty.EASY,
+        name = "Java Reflection - Attributes",
+        url = "https://www.hackerrank.com/challenges/java-reflection-attributes"
+)
+public class JavaReflectionAttributes {
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class student = Student.class;
+        Method[] methods = student.getDeclaredMethods();
+
+        ArrayList<String> methodList = new ArrayList<>();
+        for (Method m : methods) {
+            methodList.add(m.getName());
+        }
+        Collections.sort(methodList);
+        for (String name : methodList) {
+            System.out.println(name);
+        }
+    }
+}
+
+class Student {
+    private String name;
+    private String id;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}

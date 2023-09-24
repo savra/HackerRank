f7b6b31c055a9a217184b4611b71db2cf3fc4a2c
Sergey Savrukhin <savra.sv@yandex.ru>
2023-09-24 11:30:43 +0300

    Java Subarray

---
+++ src/main/java/com/hvdbs/savra/hackerrank/topics/java/JavaSubarray.java
@@ -0,0 +1,45 @@
+package com.hvdbs.savra.hackerrank.topics.java;
+
+import com.hvdbs.savra.hackerrank.statsgenerator.HackerRankInfo;
+import com.hvdbs.savra.hackerrank.statsgenerator.enums.Difficulty;
+
+import java.util.Scanner;
+
+@HackerRankInfo(
+        difficulty = Difficulty.EASY,
+        name = "Java Subarray",
+        url = "https://www.hackerrank.com/challenges/java-negative-subarray"
+)
+public class JavaSubarray {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int n = s.nextInt();
+
+        int[] a = new int[n];
+
+        int i = 0;
+
+        while(s.hasNextInt()) {
+            a[i] = s.nextInt();
+            i++;
+        }
+
+        int sum = 0;
+        int negativeSumCount = 0;
+
+        for (int j = 0; j < a.length; j++) {
+            for (int k = j; k < a.length; k++) {
+                sum += a[k];
+
+                if (sum < 0) {
+                    negativeSumCount++;
+                }
+            }
+
+            sum = 0;
+        }
+
+        System.out.println(negativeSumCount);
+    }
+}

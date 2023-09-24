826572cef521b7eaafc1302c74ba627e3399f103
Sergey Savrukhin <savra.sv@yandex.ru>
2023-09-24 07:28:30 +0300

    Java Anagrams

---
+++ src/main/java/com/hvdbs/savra/hackerrank/topics/java/JavaAnagrams.java
@@ -0,0 +1,25 @@
+package com.hvdbs.savra.hackerrank.topics.java;
+
+import com.hvdbs.savra.hackerrank.statsgenerator.HackerRankInfo;
+import com.hvdbs.savra.hackerrank.statsgenerator.enums.Difficulty;
+
+@HackerRankInfo(
+        difficulty = Difficulty.EASY,
+        name = "Java Anagrams",
+        url = "https://www.hackerrank.com/challenges/java-anagrams"
+)
+public class JavaAnagrams {
+    static boolean isAnagram(String a, String b) {
+        if (a.length() != b.length()) {
+            return false;
+        }
+
+        char[] a1 = a.toLowerCase().toCharArray();
+        char[] b1 = b.toLowerCase().toCharArray();
+
+        java.util.Arrays.sort(a1);
+        java.util.Arrays.sort(b1);
+
+        return java.util.Arrays.equals(a1, b1);
+    }
+}

887306f1dd3524521c05ab7f435bea0d31a157ff
Sergey Savrukhin <savra.sv@yandex.ru>
2023-09-24 08:18:33 +0300

    Pattern Syntax Checker

---
+++ src/main/java/com/hvdbs/savra/hackerrank/topics/java/PatternSyntaxChecker.java
@@ -0,0 +1,33 @@
+package com.hvdbs.savra.hackerrank.topics.java;
+
+import com.hvdbs.savra.hackerrank.statsgenerator.HackerRankInfo;
+import com.hvdbs.savra.hackerrank.statsgenerator.enums.Difficulty;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+@HackerRankInfo(
+        difficulty = Difficulty.EASY,
+        name = "Pattern Syntax Checker",
+        url = "https://www.hackerrank.com/challenges/pattern-syntax-checker"
+)
+public class PatternSyntaxChecker {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+
+        while(testCases > 0){
+            String pattern = in.nextLine();
+
+            try {
+                Pattern.compile(pattern);
+                System.out.println("Valid");
+            } catch(PatternSyntaxException p) {
+                System.out.println("Invalid");
+            }
+
+            testCases--;
+        }
+    }
+}

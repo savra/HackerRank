74e068b41f56b8c2d59b0d4ffdafac1af8afa749
Sergey Savrukhin <savra.sv@yandex.ru>
2023-09-24 07:55:04 +0300

    Java String Tokens

---
+++ src/main/java/com/hvdbs/savra/hackerrank/topics/java/JavaStringTokens.java
@@ -0,0 +1,33 @@
+package com.hvdbs.savra.hackerrank.topics.java;
+
+import com.hvdbs.savra.hackerrank.statsgenerator.HackerRankInfo;
+import com.hvdbs.savra.hackerrank.statsgenerator.enums.Difficulty;
+
+import java.util.Scanner;
+
+@HackerRankInfo(
+        difficulty = Difficulty.EASY,
+        name = "Java String Tokens",
+        url = "https://www.hackerrank.com/challenges/java-string-tokens"
+)
+public class JavaStringTokens {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine().trim();
+
+        if (s.isEmpty()) {
+            System.out.println(0);
+            return;
+        }
+
+        String[] res = s.split("[\\s!,?._'@]+");
+
+        System.out.println(res.length);
+
+        for (String str : res) {
+            System.out.println(str);
+        }
+
+        scan.close();
+    }
+}

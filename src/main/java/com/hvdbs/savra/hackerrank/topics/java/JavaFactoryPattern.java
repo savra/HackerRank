29346ff9ec0e8d4aec5f1b8cdc60014dfe468093
Sergey Savrukhin <savra.sv@yandex.ru>
2023-09-25 07:07:29 +0300

    Java Factory Pattern

---
+++ src/main/java/com/hvdbs/savra/hackerrank/topics/java/JavaFactoryPattern.java
@@ -0,0 +1,42 @@
+package com.hvdbs.savra.hackerrank.topics.java;
+
+import com.hvdbs.savra.hackerrank.statsgenerator.HackerRankInfo;
+import com.hvdbs.savra.hackerrank.statsgenerator.enums.Difficulty;
+
+@HackerRankInfo(
+        difficulty = Difficulty.EASY,
+        name = "Java Factory Pattern",
+        url = "https://www.hackerrank.com/challenges/java-factory"
+)
+public class JavaFactoryPattern {
+
+}
+
+interface Food {
+    public String getType();
+}
+
+class Pizza implements Food {
+    public String getType() {
+        return "Someone ordered a Fast Food!";
+    }
+}
+
+class Cake implements Food {
+
+    public String getType() {
+        return "Someone ordered a Dessert!";
+    }
+}
+
+class FoodFactory {
+    public Food getFood(String order) {
+        if ("cake".equals(order)) {
+            return new Cake();
+        } else {
+            return new Pizza();
+        }
+    }
+
+
+}

f0b0981aafb121c546a4511d2508514a0545f63e
Sergey Savrukhin <savra.sv@yandex.ru>
2023-10-19 18:13:00 +0300

    Reverse a linked list

---
+++ src/main/java/com/hvdbs/savra/hackerrank/topics/datastructures/ReverseALinkedList.java
@@ -0,0 +1,62 @@
+package com.hvdbs.savra.hackerrank.topics.datastructures;
+
+import com.hvdbs.savra.hackerrank.statsgenerator.HackerRankInfo;
+import com.hvdbs.savra.hackerrank.statsgenerator.enums.Difficulty;
+
+@HackerRankInfo(
+        difficulty = Difficulty.EASY,
+        name = "Reverse a linked list",
+        url = "https://www.hackerrank.com/challenges/reverse-a-linked-list"
+)
+public class ReverseALinkedList {
+    class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static SinglyLinkedListNode reverse(SinglyLinkedListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        SinglyLinkedListNode cur = head;
+        SinglyLinkedListNode prev = null;
+        SinglyLinkedListNode next;
+
+        while (cur != null) {
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+
+        return prev;
+    }
+}

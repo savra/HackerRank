3b422ac1a174b5ef5dab4e7ae77b6a3e6832305f
Sergey Savrukhin <savra.sv@yandex.ru>
2023-10-21 13:04:48 +0300

    Compare two linked lists

---
+++ src/main/java/com/hvdbs/savra/hackerrank/topics/datastructures/CompareTwoLinkedLists.java
@@ -0,0 +1,52 @@
+package com.hvdbs.savra.hackerrank.topics.datastructures;
+
+import com.hvdbs.savra.hackerrank.statsgenerator.HackerRankInfo;
+import com.hvdbs.savra.hackerrank.statsgenerator.enums.Difficulty;
+
+@HackerRankInfo(
+        difficulty = Difficulty.EASY,
+        name = "Compare two linked lists",
+        url = "https://www.hackerrank.com/challenges/compare-two-linked-lists")
+public class CompareTwoLinkedLists {
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    static boolean compareLists(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
+        for (SinglyLinkedListNode cur1 = head1, cur2 = head2; cur1 != null || cur2 != null; cur1 = cur1.next, cur2 = cur2.next) {
+            if (cur1 == null || cur2 == null || cur1.data != cur2.data) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
